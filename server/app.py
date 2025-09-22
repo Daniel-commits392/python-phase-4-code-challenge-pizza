baa09a1630ef29c08e6f2f002397088ed8851834
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-09-22 14:18:56 +0300

    add relationships,serialization and validation to models

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+from flask_restful import Api
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -23,6 +23,47 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+@app.route('/restaurants',methods=["GET"])
+def get_restaurants():
+    restaurants=[r.to_dict() for r in Restaurant.query.all()]
+    return jsonify(restaurants)
+
+@app.route("/restaurants/<int:id>",methods=["GET","DELETE"])
+def restaurant_by_id(id):
+    restaurant=Restaurant.query.get(id)
+    if not restaurant:
+        return {"error":"Restaurant not found"},400
+    if request.method=="GET":
+        return restaurant.to_dict()
+    if request.method=="DELETE":
+        db.session.delete(restaurant)
+        db.session.commit()
+        return "",204
+    
+
+
+
+@app.route("/pizzas",methods=["GET"])
+def get_pizzas():
+    pizzas=[p.to_dict() for p in Pizza.query.all()]
+    return jsonify(pizzas)
+
+@app.route("/restaurant_pizzas",methods=["POST"])
+def create_restaurant_pizza():
+    data=request.get_json()
+    try:
+        new_rp=RestaurantPizza(
+            price=data["price"],
+            pizza_id=data["pizza_id"],
+            restaurant_id=data["restaurant_id"],
+        )
+        db.session.add(new_rp)
+        db.session.commit()
+        return new_rp.pizza.to_dict(),201
+    except:
+        return{"errors":["validation errors"]},400   
+
 
 
 if __name__ == "__main__":
